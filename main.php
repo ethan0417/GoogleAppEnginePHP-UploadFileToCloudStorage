9ed38a56b58b25670ad65ef94eaeded5de92a778
Ethan <ethan.mail0417@gmail.com>
2014-01-09 03:42:36 +0800

    update utf-8 and big5.

--- main.php
+++ main.php
@@ -1,22 +1,32 @@
+<!DOCTYPE html>
 <html>
-  <head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8">
-  </head>
-  <body>
-    <?php
+   <head>
+      <meta charset="utf-8">
+      <meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=UTF-8">
+   </head>
+   <body>
+   <?php
+      header('Content-Type: text/plain; charset=utf-8');
+      require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+      use google\appengine\api\cloud_storage\CloudStorageTools;
 
-    require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
-    use google\appengine\api\cloud_storage\CloudStorageTools;
-
-    $options = [ 'gs_bucket_name' => 'example-images' ];
-    $upload_url = CloudStorageTools::createUploadUrl('/upload_handler.php', $options);
-    ?>
-    <form action="<?php echo $upload_url?>" enctype="multipart/form-data" method="post" accept-charset="ISO-8859-1">
-      Files to upload: <br>
-      <input type="file" name="uploaded_files" size="40">
-      <input type="submit" value="Send">
-    </form>
-  </body>
+      $options = [ 'gs_bucket_name' => 'example-images' ];
+      $upload_url = CloudStorageTools::createUploadUrl('/upload_handler.php', $options);
+   ?>
+      <script>
+      function date(){
+         var Ary = document.uploadf.uploaded_files.value.split('\\');
+         document.uploadf.fname.value=Ary[Ary.length-1];
+         return true;
+      };
+      </script>
+      <form action="<?php echo $upload_url?>" enctype="multipart/form-data" method="post" onsubmit="return date()" name='uploadf' accept-charset='ISO-8859-1'>
+         Files to upload: <br>
+         <input type="file" name="uploaded_files">
+         <input type="hidden" name="fname">
+         <input type="submit" value="Send">
+      </form>
+   </body>
 </html>
 <?
 //echo 'Hello world!';
--- upload_handler.php
+++ upload_handler.php
@@ -1,14 +1,40 @@
+<!DOCTYPE html>
 <html>
-  <head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8">
-  </head>
-  <body>
-    <?php
-      var_dump($_FILES);
-      $gs_name = $_FILES["uploaded_files"]['tmp_name'];
-      $gs_oldname = $_FILES["uploaded_files"]['name'];
-
-      move_uploaded_file($gs_name, iconv("big5", "utf-8", "gs://example-images/".$gs_oldname ));
-    ?>
-  </body>
+   <head>
+      <meta charset="utf-8">
+      <meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=UTF-8">
+   </head>
+   <body>
+<?php
+   header('Content-Type: text/plain; charset=utf-8');
+   //var_dump($_FILES);
+   echo "<pre>",print_r($_FILES),"</pre>";
+   $gs_tmpName = $_FILES["uploaded_files"]['tmp_name'];
+   $gs_originalName = mb_convert_encoding($_FILES["uploaded_files"]['name'], "UTF-8", "AUTO") ;
+   $gs_originalName2 = iconv("big5","utf-8",$_FILES["uploaded_files"]['name']) ;
+   //$test = mb_convert_encoding("我就是中文啦ddd喵喵的", "UTF-8", "AUTO") ;
+   $aaa = basename($_POST['fname']);
+   $bbb = mb_convert_encoding($aaa, "big5", "UTF-8");
+   //$aaa = basename('C:\fakepath\50桌布-1920.1220+19.jpg');
+   //$bbb = basename('/fakepath/50桌布-1920.1220+19.jpg');
+   echo "<br>";
+   echo "<br>";
+   echo $gs_originalName;
+   echo "<br>";
+   echo "<br>";
+   echo $gs_originalName2;
+   echo "<br>";
+   echo "<br>";
+   echo "<pre>",print_r($_POST),"</pre>";
+   echo "<br>";
+   echo "<br>";
+   echo $aaa;
+   echo "<br>";
+   echo "<br>";
+   echo $bbb;
+   //move_uploaded_file($gs_tmpName, iconv("utf-8", "big5", "gs://example-images/".$gs_originalName));
+   //move_uploaded_file($gs_tmpName, "gs://example-images/".$gs_originalName);
+   move_uploaded_file($gs_tmpName, "gs://example-images/".$bbb);
+?>
+   </body>
 </html>
