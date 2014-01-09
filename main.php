6e8c641cc61f193ad83ce8ea3da195362fd954f6
Ethan <ethan.mail0417@gmail.com>
2014-01-09 22:39:46 +0800

    update urlencode to file name.

--- main.php
+++ main.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
    <head>
-      <meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=ISO-8859-1">
+      <meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=UTF-8">
    </head>
    <body>
    <?php
-      //header('Content-Type: text/plain; charset=utf-8');
+      header('Content-Type: text/plain; charset=UTF-8');
       require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
       use google\appengine\api\cloud_storage\CloudStorageTools;
       $options = [ 'gs_bucket_name' => 'example-images' ];
@@ -14,11 +14,11 @@
       <script>
       function date(){
          var Ary = document.uploadf.uploaded_files.value.split('\\');
-         document.uploadf.fname.value=Ary[Ary.length-1];
+         document.uploadf.fname.value = Ary[Ary.length-1];
          return true;
       };
       </script>
-      <form action="<?php echo $upload_url?>" enctype="multipart/form-data" method="post" onsubmit="return date()" name='uploadf'>
+      <form action="<?php echo $upload_url?>" enctype="multipart/form-data" method="post" onsubmit="return date()" name='uploadf' accept-charset="ISO-8859-1">
          Files to upload: <br>
          <input type="file" name="uploaded_files">
          <input type="hidden" name="fname">
--- upload_handler.php
+++ upload_handler.php
@@ -1,24 +1,19 @@
-<!DOCTYPE html>
-<html>
-   <head>
-      <meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=utf-8">
-   </head>
-   <body>
-  <?php
-    //header('Content-Type: text/plain; charset=utf-8');
+<?php
+    header('Content-Type: text/plain; charset=UTF-8');
 
     $gs_tmpName = $_FILES["uploaded_files"]['tmp_name'];
 
     $aaa = $_POST['fname'];
-    $bbb = mb_convert_encoding($aaa, "UTF-8", "BIG5");
+    $bbb = mb_convert_encoding($aaa, "UTF-8", "AUTO");
 
     //echo "<pre>",print_r($_FILES),"</pre>";
     //echo "<pre>",print_r($_POST),"</pre>";
-    echo $aaa;
+
+    $ccc = urlencode($bbb);
+    
     echo $bbb;
-    echo "111150桌布.jpg";
-    echo "大大安安幾睡住哪";
-    move_uploaded_file($gs_tmpName, "gs://example-images/".$bbb);
-  ?>
-   </body>
-</html>
+    echo '<br>';
+    echo $ccc;    
+    
+    move_uploaded_file($gs_tmpName, "gs://example-images/".$ccc);
+?>
