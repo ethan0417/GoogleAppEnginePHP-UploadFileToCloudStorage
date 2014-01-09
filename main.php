afc18c83acf6cef57349e912267f7e200d104a99
Ethan <ethan.mail0417@gmail.com>
2014-01-09 18:08:39 +0800

    update utf-8

--- main.php
+++ main.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html>
    <head>
-      <meta charset="utf-8">
-      <meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=UTF-8">
+      <meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=ISO-8859-1">
    </head>
    <body>
    <?php
-      header('Content-Type: text/plain; charset=utf-8');
+      //header('Content-Type: text/plain; charset=utf-8');
       require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
       use google\appengine\api\cloud_storage\CloudStorageTools;
-
       $options = [ 'gs_bucket_name' => 'example-images' ];
       $upload_url = CloudStorageTools::createUploadUrl('/upload_handler.php', $options);
    ?>
@@ -20,7 +18,7 @@
          return true;
       };
       </script>
-      <form action="<?php echo $upload_url?>" enctype="multipart/form-data" method="post" onsubmit="return date()" name='uploadf' accept-charset='ISO-8859-1'>
+      <form action="<?php echo $upload_url?>" enctype="multipart/form-data" method="post" onsubmit="return date()" name='uploadf'>
          Files to upload: <br>
          <input type="file" name="uploaded_files">
          <input type="hidden" name="fname">
@@ -28,31 +26,3 @@
       </form>
    </body>
 </html>
-<?
-//echo 'Hello world!';
-/*=========================================
-require_once 'google/appengine/api/users/UserService.php';
-
-  use google\appengine\api\users\User;
-  use google\appengine\api\users\UserService;
-
-  $user = UserService::getCurrentUser();
-
-  if ($user) {
-    echo 'Hello, ' . htmlspecialchars($user->getNickname());
-  }else {
-    header('Location: ' .
-    UserService::createLoginURL($_SERVER['REQUEST_URI']));
-  };
-*/
-/*=========================================
-require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
-use google\appengine\api\cloud_storage\CloudStorageTools;
-$options = [ 'gs_bucket_name' => 'example-images' ];
-$upload_url = CloudStorageTools::createUploadUrl('/upload_handler.php', $options);
-<form action="<?php echo $upload_url?>" enctype="multipart/form-data" method="post">
-  Files to upload: <br>
-  <input type="file" name="uploaded_files" size="40">
-  <input type="submit" value="Send">
-</form>
-*/
