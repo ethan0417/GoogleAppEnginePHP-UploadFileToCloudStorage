d7e24d4a66dc0c86d2d40a31d69a2a7a136d3f5c
Ethan <ethan.mail0417@gmail.com>
2014-01-15 17:35:08 +0800

    update google cloud storage upload file for php, add method getImageServingUrl and getPublicUrl sample.

--- upload_handler.php
+++ upload_handler.php
@@ -1,19 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=UTF-8">
+  </head>
+  <body>
 <?php
-    header('Content-Type: text/plain; charset=UTF-8');
+  header('Content-Type: text/plain; charset=UTF-8');
+  
+  require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+  use google\appengine\api\cloud_storage\CloudStorageTools;
 
-    $gs_tmpName = $_FILES["uploaded_files"]['tmp_name'];
-
-    $aaa = $_POST['fname'];
-    $bbb = mb_convert_encoding($aaa, "UTF-8", "AUTO");
-
-    //echo "<pre>",print_r($_FILES),"</pre>";
-    //echo "<pre>",print_r($_POST),"</pre>";
-
-    $ccc = urlencode($bbb);
-    
-    echo $bbb;
-    echo '<br>';
-    echo $ccc;    
-    
-    move_uploaded_file($gs_tmpName, "gs://example-images/".$ccc);
+  $gs_tmpName = $_FILES["uploaded_files"]['tmp_name'];
+  $aaa = $_POST['fname'];
+  $bbb = mb_convert_encoding($aaa, "UTF-8", "AUTO");
+  //echo "<pre>",print_r($_FILES),"</pre>";
+  //echo "<pre>",print_r($_POST),"</pre>";
+  $ccc = urlencode($bbb);
+  move_uploaded_file($gs_tmpName, "gs://example-images/".$ccc);
 ?>
+<?php
+  // getImageServingUrl. get images file url, but it looks like small than original.
+  $imagesFile = CloudStorageTools::getImageServingUrl('gs://example-images/'.$ccc);
+  echo $imagesFile;
+  echo '<br>';
+?>
+<?php
+  // getPublicUrl
+  $options = [ "gs" => ["acl" => "public-read"]];
+  $ctx = stream_context_create($options);
+  file_put_contents('gs://example-images/'.$ccc , $ctx);
+  $imagesFile1 = CloudStorageTools::getPublicUrl('gs://example-images/'.$ccc , true);
+  echo $imagesFile1;
+  echo '<br>';
+?>
+  <img src='<?php echo $imagesFile ?>'>
+  <img src='<?php echo $imagesFile1 ?>'>
+  </body>
+</html>
